dad8efd9017080556e928482263e89fa2555d56b
speedygopl <damazy5@wp.pl>
2020-04-19 12:03:21 +0200

    add new method :configure to Spring Security Class

--- src/main/java/madamwebchange/security/SprSecurityConfig.java
+++ src/main/java/madamwebchange/security/SprSecurityConfig.java
@@ -2,6 +2,7 @@ package madamwebchange.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,5 +21,10 @@ public class SprSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user2").password(encoder.encode("user2Pass")).roles("USER")
                 .and()
                 .withUser("admin").password(encoder.encode("adminPass")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
     }
 }
