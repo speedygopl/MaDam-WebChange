2884f07de0e00637a8401305c5d70e7788817bfd
speedygopl <damazy5@wp.pl>
2020-04-19 11:13:47 +0200

    package names and some classes names changed

--- src/main/java/madamwebchange/MaDamRun.java
+++ src/main/java/madamwebchange/MaDamRun.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class MaDamRun {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServingWebContentApplication.class, args);
+		SpringApplication.run(MaDamRun.class, args);
 	}
 
 }
