1acfda9f1697ce8c53768780056345e9519f499b
speedygopl <damazy5@wp.pl>
2020-04-19 10:23:25 +0200

    Initial commit

---
+++ src/main/java/madamwebchange/security/SprSecurityConfig.java
@@ -0,0 +1,24 @@
+package madamwebchange.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SprSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(encoder.encode("user1Pass")).roles("USER")
+                .and()
+                .withUser("user2").password(encoder.encode("user2Pass")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("adminPass")).roles("ADMIN");
+    }
+}
